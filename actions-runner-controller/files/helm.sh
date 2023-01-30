5225fc847032c560ed7b5108d97bf4058612b744
devopstraining99 <gauravdemo06@gmail.com>
2023-01-30 19:11:41 +0530

    Cert Manager

--- actions-runner-controller/files/helm.sh
+++ actions-runner-controller/files/helm.sh
@@ -1,1 +1,4 @@
-helm install --wait --namespace $1 --create-namespace $1 actions-runner-controller/actions-runner-controller
+helm upgrade --install --namespace $1 --create-namespace\
+ --set=authSecret.create=true\
+ --set=authSecret.github_token=$2\
+ --wait actions-runner-controller actions-runner-controller/actions-runner-controller
